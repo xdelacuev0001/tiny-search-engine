52d8433e646e11b8f29dc1dc0083f2d7dcc6df02
Xenia A. Dela Cueva <f004gwf@plank.thayer.dartmouth.edu>
2021-10-22 18:13:17 -0400

    Commiting changes

---
+++ indexer/indexer.c
@@ -0,0 +1,17 @@
+/**
+ * Xenia Dela Cueva
+ * CS50 21F 
+ * TSE Lab 5-Indexer
+ * */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main, which parses arguments and initializes other modules;
+int 
+indexBuild, which builds an in-memory index from webpage files it finds in the pageDirectory;
+indexPage, which scans a webpage document to add its words to the index.
+
+//helper function
